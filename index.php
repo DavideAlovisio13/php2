aedd233ce3cd3e53c39f130bda1b21c463a832e1
DavideAlovisio13 <alovisiod@gmail.com>
2024-09-19 14:37:10 +0200

    new exercise 2

--- index.php
+++ index.php
@@ -45,7 +45,21 @@ time = 6.7---> litres = 3
 time = 11.8--> litres = 5
 */
 
-function litres(float $t): int 
+function litres(float $t): int
 {
-  return floor($t * 0.5);
+    return floor($t * 0.5);
+}
+
+/*
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+Return true if yes, false otherwise :)
+*/
+
+function hero(int $bullets, int $dragons): bool
+{
+    if ($bullets >= $dragons * 2) {
+        return true;
+    } else {
+        return false;
+    }
 }
